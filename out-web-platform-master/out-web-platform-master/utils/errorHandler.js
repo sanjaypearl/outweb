9be6eaf83dd3039d2eda33fd6a60d4da76070cfc
pearlorganisation <data-backup@pearlorganisation.com>
2025-09-23 12:36:19 +0530

    dgd

--- out-web-platform-master/out-web-platform-master/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-// utils/errorHandler.js
-export const errorHandler = (err, req, res, next) => {
-  // console.error(err.statusCode);
-
-  res.status(err.statusCode || 500).json({
-    success: false,
-    message: err.message || "Internal Server Error",
-  });
-};
